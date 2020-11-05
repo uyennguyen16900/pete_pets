2e30ee90133654e48c4bfa4a9a3de8201603cc86
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-11-04 17:56:38 -0800

    added s3-uploader middleware

--- routes/pets.js
+++ routes/pets.js
@@ -2,6 +2,35 @@
 const Pet = require('../models/pet');
 
 const mailer = require('../utils/mailer')
+
+// UPLOADING TO AWS S3
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const Upload = require('s3-uploader');
+
+const client = new Upload(process.env.S3_BUCKET, {
+  aws: {
+    path: 'pets/avatar',
+    region: process.env.S3_REGION,
+    acl:'public-read',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  },
+  cleanup: {
+    versions: true,
+    original: true
+  },
+  versions: [{
+    maxWidth: 400,
+    aspect: '16:10',
+    suffix: '-standard'
+  },{
+    maxWidth: 300,
+    aspect: '1:1',
+    suffix: '-square'
+  }]
+});
+
 // PET ROUTES
 module.exports = (app) => {
 
