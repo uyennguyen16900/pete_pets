b055685f22e2cec800bf60d32b4ec7105c2bd645
uyennguyen16900 <uyennguyen16900@gmail.com>
2020-11-16 20:52:55 -0800

    implemented responding to JSON

--- test/test-pets.js
+++ test/test-pets.js
@@ -71,7 +71,6 @@ describe('Pets', ()  => {
            done();
          });
      });
-
   });
 
   // TEST EDIT
@@ -124,6 +123,18 @@ describe('Pets', ()  => {
       .end((err, res) => {
         res.should.have.status(200);
         res.should.be.html;
+        done();
+      });
+  });
+
+  it('should list ALL pets on /pets GET', function(done) {
+    chai.request(server)
+      .get('/')
+      .set('content-type', 'application/json')
+      .end(function(err, res){
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
         done();
       });
   });
